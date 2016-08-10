fdf9f3ea64caf7921bb9d3e0eb668ecafc403faa
sekarmg <sekharau@gmail.com>
2016-08-09 22:38:53 -0400

    eslint support for vscode

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,14 @@
 import 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+import  uiRouter from 'angular-ui-router';
 import AppComponent from './app.component.js';
 import Common from './common/common.js';
 import Components from './common/common.js';
 import './app.style.css';
 
-angular.module('myApp', [
-  uiRouter,
-  Common.name,
-  Components.name
+angular.module('myApp', [ uiRouter,
+    Common.name,
+    Components.name
 ])
 .directive('app', AppComponent);
